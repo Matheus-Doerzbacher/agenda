4945eb9326382db27558dfef02dfa8492e001564
uniguacu-developer <developer@uniguacu.com.br>
2024-05-02 16:12:17 -0300

    Adicionado o Model Contact no Admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+    )
+    ordering = ('-id',)
+    # list_filter = ('first_name',)
+    search_fields = (
+        'id',
+        'first_name',
+        'last_name',
+    )
+    list_per_page = 20  # numero de resultados por pagina
+    list_editable = (
+        'last_name',
+    )  # Permite editar os campos selecionados direto na lista
+    list_display_links = (
+        'first_name',
+    )  # Permite clicar nos campos da lista para editar
