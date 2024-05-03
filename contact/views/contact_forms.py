48b18f3241e9316c0dce3cee7622890911ecb0dc
uniguacu-developer <developer@uniguacu.com.br>
2024-05-03 15:11:31 -0300

    Verificando form.is_valid, salvando com form.save e redirecionando a página

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from ..forms import ContactForm
 
 
 def create(request):
     if request.method == 'POST':
+        form = ContactForm(request.POST)
+
         context = {
-            'form': ContactForm(request.POST),
+            'form': form,
         }
+
+        if form.is_valid():
+            # Para salvar direto no banco
+            # form.save()
+
+            # Quando se deseja alterar algum dado antes de salvar no banco
+            contact = form.save(commit=False)
+            contact.first_name = contact.first_name.strip()
+            contact.last_name = contact.last_name.strip()
+            contact.save()
+            return redirect('contact:create')
 
         return render(request, 'contact/create.html', context)
 
