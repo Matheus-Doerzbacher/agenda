313e88f1e6c2520b335bb6efe3fa80fdbcee3fc3
uniguacu-developer <developer@uniguacu.com.br>
2024-05-03 15:32:15 -0300

    Usando instance do ModelForm para atualizar dados de um contato

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, contact, search, create
+from .views import index, contact, search, create, update
 
 app_name = 'contact'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     # contact (CRUD)
     path('contact/<int:contact_id>/detail/', contact, name='contact'),
     path('contact/create/', create, name='create'),
+    path('contact/<int:contact_id>/update/', update, name='update'),
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from ..forms import ContactForm
+from ..models import Contact
 
 
 def create(request):
+    form_action = reverse('contact:create')
     if request.method == 'POST':
         form = ContactForm(request.POST)
 
         context = {
             'form': form,
+            'form_action': form_action,
         }
 
         if form.is_valid():
@@ -19,12 +23,46 @@ def create(request):
             contact.first_name = contact.first_name.strip()
             contact.last_name = contact.last_name.strip()
             contact.save()
-            return redirect('contact:create')
+            return redirect('contact:update', contact_id=contact.id)
 
         return render(request, 'contact/create.html', context)
 
     context = {
         'form': ContactForm(),
+        'form_action': form_action,
+    }
+
+    return render(request, 'contact/create.html', context)
+
+
+def update(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id, show=True)
+    form_action = reverse('contact:update', args=(contact_id,))
+
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+
+        context = {
+            'form': form,
+            'form_action': form_action,
+        }
+
+        if form.is_valid():
+            # Para salvar direto no banco
+            # form.save()
+
+            # Quando se deseja alterar algum dado antes de salvar no banco
+            contact = form.save(commit=False)
+            contact.first_name = contact.first_name.strip()
+            contact.last_name = contact.last_name.strip()
+            contact.save()
+            return redirect('contact:update', contact_id=contact.id)
+
+        return render(request, 'contact/create.html', context)
+
+    context = {
+        'form': ContactForm(instance=contact),
+        'form_action': form_action,
     }
 
     return render(request, 'contact/create.html', context)
