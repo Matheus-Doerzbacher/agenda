42ae5e74dfa2e5be8541749a74897e7c5f1c5033
uniguacu-developer <developer@uniguacu.com.br>
2024-05-06 14:04:30 -0300

    Usando UserCreationForm para criar novos usuários no Django

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Contact
 
 
@@ -82,3 +84,30 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(min_length=3, required=True)
+    last_name = forms.CharField(min_length=3, required=True)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+            'password1',
+            'password2',
+        )
+        unique_together = ('email',)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Email já cadastrado', code='invalid'),
+            )
